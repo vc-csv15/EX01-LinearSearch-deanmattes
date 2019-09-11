b8cdd39790d7f47f649bfc3a91eb4669c294582c
blakemeschke <blake_meschke@my.vcccd.edu>
2019-09-11 14:36:26 -0700

    Wrote the code.

---
+++ src/linearsearch.cc
@@ -0,0 +1,18 @@
+//
+// Created by vscilab on 9/9/2019.
+//
+#include <cstdint>
+#include <cstddef>
+
+namespace edu {namespace vcccd { namespace vc {namespace csv15{
+template <class T>
+
+int64_t find(const T& value, T array[], size_t size)
+{
+    if (array == nullptr) return -1;
+    for (size_t i = 0; i < size; i++)
+    {
+        if (value == array [i]) return i;
+    }
+    return -1;
+}}}}}
